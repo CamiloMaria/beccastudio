aab1748f60aa6da1dd42c259613de5b88ed3f99f
CamiloMaria <camilocraft08@gmail.com>
2025-05-16 14:43:36 -0400

    Add Footer component to enhance site navigation and user engagement. Update Layout to include Footer and modify index.ts to export Footer component. Remove unused Button import from Contact section for improved code clarity.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header.astro";
 export { default as Button } from "./Button.astro";
 export { default as StarRating } from "./StarRating.astro";
+export { default as Footer } from "./Footer.astro";
